f79dccfc9d7f046e4c77add3943e554ef06d0fea
sauravk7077 <sauravk7077@gmail.com>
2020-09-07 09:55:06 +0530

    added page

---
+++ precache-manifest.171cf906a27ac4df9b7787448e1a7219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43a2d0f021a04ddeef20b688a4ea65a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec4f83b1f52aab99d656",
+    "url": "./static/css/main.b8e0e344.chunk.css"
+  },
+  {
+    "revision": "fc2d524d79bf0429bd65",
+    "url": "./static/js/2.004ff0f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.004ff0f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4f83b1f52aab99d656",
+    "url": "./static/js/main.c119fd7b.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
--- src/ToDo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function ToDo(props) {
-    return (
-        <div className="toDoElement" onClick={props.onClick} id={props.id}>{props.t}</div>
-    );
-}
-
-export default ToDo;
